e5bcf78ec64a3fffbed9e57a7296c59b40184f45
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-06-13 13:25:49 +0000

    )

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -25,19 +25,19 @@ class Server:
         return self.__dataset
 
     def get_page(
-        self,
-        page: int = 1,
-        page_size: int = 10
-    ) -> List[List]:
-        """Return a page of the dataset"""
+    self,
+    page: int = 1,
+    page_size: int = 10
+) -> List[List]:
+    """Return a page of the dataset"""
 
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
+    assert isinstance(page, int) and page > 0
+    assert isinstance(page_size, int) and page_size > 0
 
-        start, end = index_range(page, page_size)
-        data = self.dataset()
+    start, end = index_range(page, page_size)
+    data = self.dataset()
 
-        if start >= len(data):
-            return []
+    if start >= len(data):
+        return []
 
-        return data[start:end]
+    return data[start:end]
