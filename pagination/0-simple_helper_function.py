89d5a23815bae5f0a11c834a7ee75d261dba74eb
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-06-13 11:59:50 +0000

    )

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,5 +5,5 @@
 def index_range(page: int, page_size: int) -> tuple:
     '''simple helper function'''
     end = page * page_size
-    start = end - page_size
+    start = (page - 1) * page_size
     return (start, end)
