f3d6815b2b3f1aebd51b51425c73dc3a01347979
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-06-13 06:11:58 +0000

    )

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 def index_range(page, page_size):
-    start_index = (page -1 ) * page_size
+    start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)
