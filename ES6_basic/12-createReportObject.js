8b91ac5e0098e39cff2366f9db56afd953e3ecab
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-06-28 23:04:00 +0400

    Create report object with employee data and methods

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: { ...employeesList },
+
+    getNumberOfDepartments(employees) {
+      return Object.keys(employees).length;
+    },
+  };
+}
