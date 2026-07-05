18f62de57506c57c5282b913b8a089393fd0c88c
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-07-05 19:58:23 +0000

    )

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,31 @@
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        this._name = name;
+    }
+
+    get length() {
+        return this._length;
+    }
+
+    set length(length) {
+        this._length = length;
+    }
+
+    get students() {
+        return this._students;
+    }
+
+    set students(students) {
+        this._students = students;
+    }
+}
