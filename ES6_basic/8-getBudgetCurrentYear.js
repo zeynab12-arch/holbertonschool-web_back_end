79170126292525e8c2a4dd594e18dafee6a589e7
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-07-19 20:10:05 +0000

    O

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -4,11 +4,11 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const year = getCurrentYear();
-
-  return {
-    [`income-${year}`]: income,
-    [`gdp-${year}`]: gdp,
-    [`capita-${year}`]: capita,
+  const budget = {
+    [`income-${getCurrentYear()}`] : income;
+    [`gdp-${getCurrentYear()}`] : gdp;
+    [`capita-${getCurrentYear()}`] : capita;
   };
+
+  return budget;
 }
