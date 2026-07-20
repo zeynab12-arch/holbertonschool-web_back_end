90388266c15369cff8c14dcc723e5c51dedd756f
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-07-20 07:40:37 +0000

    )

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,9 +1,8 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: { ...employeesList },
-
-    getNumberOfDepartments(employees) {
-      return Object.keys(employees).length;
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
     },
   };
 }
