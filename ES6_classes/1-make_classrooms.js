9eb0e9b3279e8d08adc9ab23fee17784b875bd2c
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-07-20 08:58:59 +0000

    )

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,5 +1,7 @@
 import ClassRoom from "./0-classroom.js";
-
 export default function initializeRooms() {
-    return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)]
+    const obj1 = new ClassRoom(19);
+    const obj2 = new ClassRoom(20);
+    const obj3 = new ClassRoom(34);
+    return [...obj1, ...obj2, ...obj3]
 }
