fca1224fff559a2aa0cc93a400d76ac0cc8a000f
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-07-27 20:33:27 +0000

    )

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -2,10 +2,8 @@ export default function guardrail(mathFunction) {
     const queue = [];
   
     try {
-      const result = mathFunction();
-      queue.push(result);
+      queue.push(mathFunction());
     } catch (error) {
-      // Converts the error object to its string representation ("Error: message")
       queue.push(error.toString());
     } finally {
       queue.push('Guardrail was processed');
