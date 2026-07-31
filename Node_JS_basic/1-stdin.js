8f1babeb768a05bc7e42e6e7bbbcedeb64157625
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-07-31 15:43:45 +0000

    )

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write("Welcome to Holberton School, what is your name?\n")
 process.stdin.on("data", (inputname) => {
-  process.stdout(`Your name is: ${inputname.toString().trim()\n}`);
+  process.stdout.write(`Your name is: ${inputname.toString().trim()}\n`);
 });
 process.stdin.on("end", () => {
   process.stdout.write("This important software is now closing\n");
