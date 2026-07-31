fe9587acaba2f02ef2b19705ea8f6c014fc2a0aa
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-07-31 15:57:02 +0000

    )

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,3 +1,4 @@
+process.stdin.resume();
 process.stdout.write("Welcome to Holberton School, what is your name?\n")
 process.stdin.on("data", (inputname) => {
   process.stdout.write(`Your name is: ${inputname.toString().trim()}\n`);
