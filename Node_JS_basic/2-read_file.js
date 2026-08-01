047d279e17d2ef7fc364349f088805535dcd60fe
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-08-01 12:09:12 +0000

    )

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,31 +1,38 @@
 const fs = require('fs');
+
 function countStudents(path) {
   let data;
 
   try {
     data = fs.readFileSync(path, 'utf8');
   } catch (err) {
-    throw new Error('Cannot load the database')
+    throw new Error('Cannot load the database');
   }
 
   const lines = data.split('\n').filter((line) => line.trim() !== '');
   const students = lines.slice(1);
+
   console.log(`Number of students: ${students.length}`);
 
   const fields = {};
+
   students.forEach((student) => {
     const parts = student.split(',');
     const firstname = parts[0];
-    const field = parts[3]
+    const field = parts[3];
+
     if (!fields[field]) {
-      fields[field] = []
+      fields[field] = [];
     }
 
     fields[field].push(firstname);
-  })
+  });
+
   Object.keys(fields).forEach((field) => {
     console.log(
       `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
     );
   });
+}
+
 module.exports = countStudents;
