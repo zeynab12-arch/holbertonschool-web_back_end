245fff2748847718461eef6fa3cd1813f10e05e4
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-08-01 12:11:34 +0000

    )

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -30,7 +30,7 @@ function countStudents(path) {
 
   Object.keys(fields).forEach((field) => {
     console.log(
-      `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+      `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`,
     );
   });
 }
