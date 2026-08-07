bcf03c4b7ddb32cce28af3e8d33ed5fad939fe2b
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-08-07 10:13:12 +0000

    )

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-'''Async Comprehension'''
+"""
+This module provides async_comprehension function.
+"""
+import asyncio
 from typing import List
+
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    '''Async comprehension'''
-    return [i async for i in async_generator()]
+    """
+    This function takes no argument and collects 10 random numbers using
+    async comprehesing over async_generator, returns 10 random numbers.
+    """
+    result = [i async for i in async_generator()]
+    return result
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
-'''Async Comprehension'''
+"""
+This module provides a function for parallel comprehensions.
+"""
 import asyncio
-from typing import List
+import time
+
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    '''Measure runtime'''
-    start = asyncio.get_event_loop().time()
-    await asyncio.gather(*[async_comprehension() for _ in range(4)])
-    end = asyncio.get_event_loop().time()
-    return end - start
+    """
+    This function will execute async_comprehension 4 times in parallel
+    using asyncio.gather and return the total run time.
+    """
+    start_time = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
+    return total_time
